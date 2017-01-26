e5d33edae5ca9d84c605417f848aecfb3e5459d8
l3kov9 <l3kov.9@gmail.com>
2017-01-26 13:07:45 +0200

    Finish DataTypes Project

---
+++ DataTypes-NumeralTypesAndConversion/03.ExactProductOfRealNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.ExactProductOfRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sum=1;
+            for (int i = 0; i < n; i++)
+            {
+                sum *= decimal.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ DataTypes-NumeralTypesAndConversion/04.Transport/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Transport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int persons = int.Parse(Console.ReadLine());
+
+            int capacity = 24;
+            int courses = (int) (Math.Ceiling((double)persons / capacity));
+            Console.WriteLine(courses);
+        }
+    }
+}
