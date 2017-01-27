5209ed8773663dd865c203648e0c9ba237f74cb7
l3kov9 <l3kov.9@gmail.com>
2017-01-27 10:14:36 +0200

    Upload DataTypes-TextAndVariables project

---
+++ DataTypes-TextAndVariables/01.SpecialNumbers/SpecialNumbers.cs
@@ -0,0 +1,21 @@
+﻿namespace _01.SpecialNumbers
+{
+    using System;
+    public class SpecialNumbers
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int sum = 0;
+                foreach (var item in i.ToString())
+                {
+                    sum += int.Parse(item.ToString());
+                }
+                Console.WriteLine("{0} -> {1}",i,sum==5 || sum==7 || sum==11);
+            }
+        }
+    }
+}
---
+++ DataTypes-TextAndVariables/04.RefactorVolumeOfPyramid/RefactorVolumePyramid.cs
@@ -0,0 +1,18 @@
+﻿namespace _04.RefactorVolumeOfPyramid
+{
+    using System;
+    public class RefactorVolumePyramid
+    {
+        public static void Main()
+        {
+            Console.Write("Length: ");
+            double length = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            double heigth = double.Parse(Console.ReadLine());
+            double volume = length * width * heigth/(double)3;
+            Console.WriteLine("Pyramid Volume: {0:F2}", volume);
+        }
+    }
+}
