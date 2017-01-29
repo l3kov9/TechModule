34205a249f39c3a07644745af3d49f44f80edb0f
l3kov9 <l3kov.9@gmail.com>
2017-01-29 06:20:21 +0200

    Update DataTypesAndVariables-Exercises

---
+++ DataTypesAndVariables-Exercises/19.TheaThePhotographer/TheaThePhotographer.cs
@@ -0,0 +1,19 @@
+﻿namespace _19.TheaThePhotographer
+{
+    using System;
+    public class TheaThePhotographer
+    {
+        public static void Main()
+        {
+            long countPictures = long.Parse(Console.ReadLine());
+            long filterTimePic = long.Parse(Console.ReadLine());
+            int percentageGoodPics = int.Parse(Console.ReadLine());
+            long uploadTimePic = long.Parse(Console.ReadLine());
+
+            long totalTime = countPictures * filterTimePic + ((long)Math.Ceiling(percentageGoodPics / 100.0 *countPictures)*uploadTimePic);
+
+            TimeSpan t = TimeSpan.FromSeconds(totalTime);
+            Console.WriteLine("{0:D1}:{1:D2}:{2:D2}:{3:D2}",t.Days,t.Hours,t.Minutes,t.Seconds);
+        }
+    }
+}
